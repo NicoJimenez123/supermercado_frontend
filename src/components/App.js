d527c45ba89a09d3bdf5e70f184e2ba538ee2fe4
Nicolas Jimenez <Nicolasdanieljimenez@hotmail.com>
2023-05-26 19:50:47 -0300

    Añadido componente AgregarProducto

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { ListadoProductos } from "./ListadoProductos";
+import { AgregarProducto } from "./AgregarProducto";
 
 const App = () => {
   return (
     <div>
       <h1>Listado de Productos</h1>
       <ListadoProductos />
+      <AgregarProducto />
     </div>
   );
 }
