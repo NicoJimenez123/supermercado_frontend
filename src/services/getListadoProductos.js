6b12a711deec60610a4df23b3ee45d525d81d449
Nicolas Jimenez <Nicolasdanieljimenez@hotmail.com>
2023-05-26 19:29:07 -0300

    Eliminado servicio sin usaer

--- src/services/getListadoProductos.js
+++
@@ -1,7 +0,0 @@
-const getListadoProductos = async () => {
-  const { apiBackend } = require('./config')
-  await apiBackend().get('/tienda/productos')
-    .then(res => res.data)
-}
-
-export { getListadoProductos }
