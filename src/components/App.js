13bde1d65d2b42516639058cee60f6943716e8fc
Nicolas Jimenez <Nicolasdanieljimenez@hotmail.com>
2023-05-26 20:54:25 -0300

    Añadida responsabilidad de obtener el listado de productos

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ListadoProductos } from "./ListadoProductos";
 import { AgregarProducto } from "./AgregarProducto";
 
 const App = () => {
+  const [ listado, setListado ] = useState()
+  const listarProductos = async() => {
+    const response = await fetch(`http://${process.env.REACT_APP_BACKEND_IP}/tienda/productos`)
+                            .then(res => res.json())
+    setListado(response)
+  }
+
+  useEffect(() => {
+    listarProductos()
+  }, [])
+
   return (
     <div>
       <h1>Listado de Productos</h1>
-      <ListadoProductos />
-      <AgregarProducto />
+      <AgregarProducto listarProductos={listarProductos}/>
+      <ListadoProductos listadoProductos={listado} listarProductos={listarProductos}/>
     </div>
   );
 }
