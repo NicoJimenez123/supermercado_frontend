216508ec55602f7eb0cf3dbbe47169dfcbc5fb48
Nicolas Jimenez <Nicolasdanieljimenez@hotmail.com>
2023-06-03 17:00:30 -0300

    Creado componente que se encarga de manipular la lista

---
+++ src/components/ModificarListado.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+const listadoDePropiedades = [
+  {propiedad: "nombre", propiedad_nombre: "Nombre"},
+  {propiedad: "prese", propiedad_nombre: "Peso"},
+  {propiedad: "precio", propiedad_nombre: "Precio"},
+  {propiedad: "stock", propiedad_nombre: "Stock"},
+]
+
+const ModificarListado = ({setListado, listadoProductos, listarProductos}) => {
+  const [ propiedad, setPropiedad] = useState(listadoDePropiedades[0].propiedad) // Seteo la 1ra propiedad como predeterminada
+  const [ valorFiltro, setValorFiltro] = useState("")
+
+  const filtrar = () => {
+    const listaFiltrada = listadoProductos.filter(p => {
+      return p[propiedad].includes(valorFiltro)
+    })
+    setListado(listaFiltrada)
+  }
+
+  const reiniciarFiltros = () => {
+    listarProductos()
+  }
+
+  return (
+    <div>
+      <select name="propiedades" id='propiedades' onChange={e => setPropiedad(e.target.value)}>
+        {
+          listadoDePropiedades.map((p, index) =>
+            <option key={index} value={p.propiedad}>{p.propiedad_nombre}</option>)
+        }
+      </select>
+      <input name="valorFiltro" placeholder='Ingresar Valor' value={valorFiltro} onChange={e => setValorFiltro(e.target.value)}/>
+      <br></br>
+      <button onClick={filtrar}>Filtrar</button>
+      <button onClick={reiniciarFiltros}>Reiniciar Filtrado</button>
+    </div>
+  )
+}
+
+export { ModificarListado }
