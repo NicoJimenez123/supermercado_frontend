dab3ae7fbd432888387a6d3ef47580667dccfcb3
Nicolas Jimenez <Nicolasdanieljimenez@hotmail.com>
2023-06-11 14:31:51 -0300

    Creado archivo

---
+++ src/funciones/carritoHandler.js
@@ -0,0 +1,21 @@
+const anadirProducto = (producto) => {
+
+}
+
+const eliminarProducto = (producto) => {
+
+}
+
+const listarCarrito = () => {
+
+}
+
+const incrementarUnidadesProducto = (producto) => {
+
+}
+
+const decrementarUnidadesProducto = (producto) => {
+
+}
+
+export { anadirProducto, eliminarProducto, listarCarrito, incrementarUnidadesProducto, decrementarUnidadesProducto }
