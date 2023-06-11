e70f63b76f61728b41265af38a590b80c52812df
Nicolas Jimenez <Nicolasdanieljimenez@hotmail.com>
2023-06-11 14:43:00 -0300

    Añadido componente carrito y agrupados los demas componentes dentro de un div

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import '../estilos/App.css'
 import { ListadoProductos } from "./ListadoProductos";
 import { AgregarProducto } from "./AgregarProducto";
 import { ModificarListado } from "./ModificarListado";
+import { CarritoProducto } from "./CarritoProducto";
 
 const App = () => {
   const [ listado, setListado ] = useState()
@@ -18,17 +19,20 @@ const App = () => {
 
   return (
     <div id="app">
-      <h1>Modificar Listado de Productos</h1>
-      <div id="modificarListado">
-        <div id="agregarProducto">
-          <AgregarProducto listarProductos={listarProductos}/>
+      <CarritoProducto />
+      <div id="main">
+        <h1>Modificar Listado de Productos</h1>
+        <div id="modificarListado">
+          <div id="agregarProducto">
+            <AgregarProducto listarProductos={listarProductos}/>
+          </div>
+          <div id="modListado">
+            <ModificarListado setListado={setListado} listadoProductos={listado} listarProductos={listarProductos}/>
+          </div>
         </div>
-        <div id="modListado">
-          <ModificarListado setListado={setListado} listadoProductos={listado} listarProductos={listarProductos}/>
-        </div>
+        <h1>Listado de Productos</h1>
+        <ListadoProductos listadoProductos={listado} listarProductos={listarProductos}/>
       </div>
-      <h1>Listado de Productos</h1>
-      <ListadoProductos listadoProductos={listado} listarProductos={listarProductos}/>
     </div>
   );
 }
