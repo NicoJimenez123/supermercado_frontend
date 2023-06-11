dbabce50b43403c582b945d572c2a7ddf63d02d7
Nicolas Jimenez <Nicolasdanieljimenez@hotmail.com>
2023-06-11 15:26:41 -0300

    Creado html del carrito

--- src/components/CarritoProducto.js
+++ src/components/CarritoProducto.js
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from 'react'
+import * as carritoHandler from '../funciones/carritoHandler'
 
-const CarritoProducto = () => {
-
+const CarritoProducto = ({listadoCarrito, actualizarCarrito}) => {
   return (
     <div id="CarritoProducto">
       <h2>
         Carrito
       </h2>
+      <div>
+        {
+          !listadoCarrito?.length > 0
+            ? <div>No hay Productos</div>
+            : listadoCarrito?.map((producto, index) => (
+              <div key={index}>
+                <div>
+                  <b>Producto</b>: {producto._id}
+                </div>
+                <button onClick={() => carritoHandler.eliminarProducto(producto, actualizarCarrito)}>Eliminar</button>
+              </div>
+            ))
+        }
+      </div>
+      <button onClick={() => carritoHandler.eliminarCarrito(actualizarCarrito)}>Eliminar Carrito</button>
     </div>
   ) 
 }
