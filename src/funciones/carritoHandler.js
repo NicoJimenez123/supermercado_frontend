e246cdd31992692d895761889b672105cfeef461
Nicolas Jimenez <Nicolasdanieljimenez@hotmail.com>
2023-06-11 14:43:36 -0300

    Añadidos console.log para ver si andaban las funciones

--- src/funciones/carritoHandler.js
+++ src/funciones/carritoHandler.js
@@ -1,21 +1,23 @@
-const anadirProducto = (producto) => {
+import { listadoDePropiedades } from "../services/config"
 
+const anadirProducto = (producto) => {
+  console.log("Añadir Producto: ", producto)
 }
 
 const eliminarProducto = (producto) => {
-
+  console.log("Eliminar Producto: ", producto)
 }
 
 const listarCarrito = () => {
-
+  console.log("Listar Productos")
 }
 
 const incrementarUnidadesProducto = (producto) => {
-
+  console.log("Incrementar Producto: ", producto)
 }
 
 const decrementarUnidadesProducto = (producto) => {
-
+  console.log("Decrementar Producto: ", producto)
 }
 
 export { anadirProducto, eliminarProducto, listarCarrito, incrementarUnidadesProducto, decrementarUnidadesProducto }
